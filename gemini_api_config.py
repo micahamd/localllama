f3b9459f5a775aad8acb1ef6eb4b1215fe39cdfb
Micah Amd <76642328+micahamd@users.noreply.github.com>
2025-06-10 11:48:36 +0600

    enabled markitdown image processing

--- gemini_api_config.py
+++ gemini_api_config.py
@@ -17,16 +17,15 @@ class GeminiAPIConfig:
         self._text_models = [
             "gemini-2.0-flash",
             "gemini-2.0-flash-lite",
-            "gemini-2.5-flash-preview-04-17",
-            "gemini-2.0-flash-thinking-exp-01-21",
-            "gemini-2.5-pro-exp-03-25"
+            "gemini-2.5-flash-preview-05-20",
+            "gemini-2.5-pro-preview-06-05"
         ]
 
         self._vision_models = [
             "gemini-2.0-flash",
             "gemini-2.0-flash-lite",
-            "gemini-2.0-flash-thinking-exp-01-21",
-            "gemini-2.5-pro-exp-03-25"
+            "gemini-2.5-flash-preview-05-20",
+            "gemini-2.5-pro-preview-06-05"
         ]
 
         self._image_generation_models = [
